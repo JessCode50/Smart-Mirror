effdef013f772de0e6589cd3f422df322b8d40aa
Elizabeth Wright <e3wright@uwaterloo.ca>
2024-11-03 15:17:11 -0500

    basic news format

--- next-ui-dashboard-app/app/modules/News.tsx
+++ next-ui-dashboard-app/app/modules/News.tsx
@@ -1,5 +1,16 @@
 const News = () => {
-  return <div>clock</div>
+  return <div><h6 className="text-xs"> source </h6>
+  <h1 className="text-xl font-bold">NEWS TITLE HERE</h1>
+  <h2 className="text-sm">SUBTITLE HERE</h2>
+  <br></br>
+  <h6 className="text-xs"> source </h6>
+  <h1 className="text-xl font-bold">NEWS TITLE HERE</h1>
+  <h2 className="text-sm">SUBTITLE HERE</h2>
+  <br></br>
+  <h6 className="text-xs"> source </h6>
+  <h1 className="text-xl font-bold">NEWS TITLE HERE</h1>
+  <h2 className="text-sm">SUBTITLE HERE</h2>
+  </div>
 }
 
 export default News
