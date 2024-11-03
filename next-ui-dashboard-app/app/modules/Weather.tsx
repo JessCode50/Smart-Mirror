33427302f51121171010c13bc189d5e33ff6676d
Jessica <j2kovac@uwaterloo.ca>
2024-11-03 15:48:39 -0500

    Added weather display outline

--- next-ui-dashboard-app/app/modules/Weather.tsx
+++ next-ui-dashboard-app/app/modules/Weather.tsx
@@ -1,5 +1,15 @@
 const Weather = () => {
-  return <div>clock</div>
+  return (
+    <div>
+      <h1 className="text-2xl">Weather</h1>
+      <p>Current Weather:</p>
+      <p>Temperature: 33ºC</p>
+      <p>Wind: 21 km/h</p>
+      {/* <div className="px-8 ...">Morning: Afternoon:</div> */} 
+      <p>Morning: Afternoon: Evening:</p>
+      <p>Temperature: 13ºC Temperature: 25ºC Temperature: 0ºC</p>
+    </div>
+  )
 }
 
 export default Weather
