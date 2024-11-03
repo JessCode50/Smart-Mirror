42895d51f33d7215d521da9b31e5a014e714f56d
Elizabeth Wright <e3wright@uwaterloo.ca>
2024-11-03 15:17:11 -0500

    basic news format

--- next-ui-dashboard-app/app/modules/News.tsx
+++ next-ui-dashboard-app/app/modules/News.tsx
@@ -1,32 +1,16 @@
-const News = async () => {
-  const data = await fetch(
-    "https://newsdata.io/api/1/latest?apikey=pub_585756761adc3fda997c187e7ec49def8c62e&language=en&country=ca&category=sports,technology,education&prioritydomain=top&size=5&video=0&image=0&removeduplicate=1&excludedomain=news.google.com,9to5google.com"
-  )
-
-  interface ArticleInfo {
-    source_name: string
-    title: string
-  }
-  const articles: {
-    results: Array<ArticleInfo>
-  } = await data.json()
-
-  return (
-    <div>
-      <div className="grid grid-cols-1 gap-4 max-w-80">
-        <div className="flex justify-center">
-          <h1 className="text-4xl font-bold">NEWS</h1>
-        </div>
-        {articles.results.map((article: ArticleInfo, i: number) => (
-          <article key={i} className="text-wrap">
-            <h6 className="text-s">{article.source_name}</h6>
-            <h1 className="text-xl font-bold">{article.title}</h1>
-            <br></br>
-          </article>
-        ))}
-      </div>
-    </div>
-  )
+const News = () => {
+  return <div><h6 className="text-xs"> source </h6>
+  <h1 className="text-xl font-bold">NEWS TITLE HERE</h1>
+  <h2 className="text-sm">SUBTITLE HERE</h2>
+  <br></br>
+  <h6 className="text-xs"> source </h6>
+  <h1 className="text-xl font-bold">NEWS TITLE HERE</h1>
+  <h2 className="text-sm">SUBTITLE HERE</h2>
+  <br></br>
+  <h6 className="text-xs"> source </h6>
+  <h1 className="text-xl font-bold">NEWS TITLE HERE</h1>
+  <h2 className="text-sm">SUBTITLE HERE</h2>
+  </div>
 }
 
 export default News
