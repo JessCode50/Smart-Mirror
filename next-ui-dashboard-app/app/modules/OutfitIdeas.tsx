344aa05f42ec8a0c7b2c68103ea4a1a6c1757683
Chris Yang <chrisyx511@gmail.com>
2024-11-05 13:56:16 -0500

    Formatted OpenAI output and redid input

--- next-ui-dashboard-app/app/modules/OutfitIdeas.tsx
+++ next-ui-dashboard-app/app/modules/OutfitIdeas.tsx
@@ -5,25 +5,49 @@ const OutfitIdeas = async () => {
     apiKey: process.env.OPENAI_API_KEY,
   })
 
+  const aiQueryData = {
+    weather: {
+      temperature: "24",
+      unit: "celcius",
+      location: "Waterloo, ON, Canada",
+      windSpeed: "24 km/h",
+      humidex: "60",
+    },
+    gender: "Unisex",
+    style: "Streetwear",
+  }
+
   const completion = await openai.chat.completions.create({
-    model: "gpt-4o",
+    model: "gpt-4o-mini",
     messages: [
       {
         role: "system",
         content: `You are an assistant designed to provide an average person general outfit 
         deas based on parameters specified in the user prompt. Those parameters may include things such as weather, 
-        preferred styles, gender / unisex. Provide 2 or 3 ideas only in an array of strings, in JSON without any formatting.`,
+        preferred styles, gender / unisex and will be provided as a JSON object of strings. 
+        Provide exactly three ideas that are appropriate for the conditions provided only in an array of strings, 
+        in JSON without any formatting.`,
       },
       {
         role: "user",
-        content:
-          "Weather: 24 degrees celcius, sunny. Gender: Unisex. No other requirement.",
+        content: JSON.stringify(aiQueryData),
       },
     ],
   })
-  const result = JSON.parse(completion.choices[0].message.content || "")
 
-  return <div>{result[0]}</div>
+  const result: Array<string> | null = JSON.parse(
+    completion.choices[0].message.content || "null",
+  )
+  return result ? (
+    <div className="m-4 p-2">
+      <h1 className=" text-xl ">Outfit Ideas</h1>
+      <li>{result[0] || ""}</li>
+      <li>{result[1] || ""}</li>
+      <li>{result[2] || ""}</li>
+    </div>
+  ) : (
+    <div></div>
+  )
 }
 
 export default OutfitIdeas
--- next-ui-dashboard-app/app/page.tsx
+++ next-ui-dashboard-app/app/page.tsx
@@ -3,14 +3,17 @@ import Clock from "./modules/Clock"
 import News from "./modules/News"
 import OutfitIdeas from "./modules/OutfitIdeas"
 import Weather from "./modules/Weather"
+import { Suspense } from "react"
 
 export default function Home() {
   return (
     <div>
       <Clock></Clock>
       <News></News>
-      <OutfitIdeas></OutfitIdeas>
       <Weather></Weather>
+      <Suspense>
+        <OutfitIdeas></OutfitIdeas>
+      </Suspense>
     </div>
   )
 }
