6ecc2d1ea4e8e66725b4e24253139281fda6b30c
Chris Yang <chrisyx511@gmail.com>
2024-11-15 18:07:46 -0500

    Fixed rendering bug due to missing key property

--- next-ui-dashboard-app/app/modules/News.tsx
+++ next-ui-dashboard-app/app/modules/News.tsx
@@ -1,22 +1,18 @@
 const News = async () => {
   const data = await fetch(
-    "https://newsdata.io/api/1/latest?apikey=pub_585756761adc3fda997c187e7ec49def8c62e&language=en&country=ca&category=sports,technology,education&prioritydomain=top&size=3&video=0&image=0&removeduplicate=1&excludedomain=news.google.com,9to5google.com",
+    "https://newsdata.io/api/1/latest?apikey=pub_585756761adc3fda997c187e7ec49def8c62e&language=en&country=ca&category=sports,technology,education&prioritydomain=top&size=3&video=0&image=0&removeduplicate=1&excludedomain=news.google.com,9to5google.com"
   )
-  const articles:any = await data.json()
+  const articles: any = await data.json()
 
   return (
     <div className="grid grid-cols-1 gap-4 max-w-80">
-      {
-        articles.results.map(article => (
-          
-          <article className="text-wrap">
-            <h6 className="text-s">{article.source_name}</h6>
-            <h1 className="text-xl font-bold">{article.title}</h1>
-            <br></br>
-          </article>
-        ))
-      }
-      
+      {articles.results.map((article: any, i: number) => (
+        <article key={i} className="text-wrap">
+          <h6 className="text-s">{article.source_name}</h6>
+          <h1 className="text-xl font-bold">{article.title}</h1>
+          <br></br>
+        </article>
+      ))}
     </div>
   )
 }
