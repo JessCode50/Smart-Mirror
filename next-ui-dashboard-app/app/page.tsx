7f49fafb411b574a7bdcce2fdea7a802ef056d1d
Fiona Dang <f2dang@uwaterloo.ca>
2024-11-25 17:59:21 -0500

    Fiona/global styling

--- next-ui-dashboard-app/app/page.tsx
+++ next-ui-dashboard-app/app/page.tsx
@@ -8,14 +8,20 @@ import Photos from "./modules/Photos"
 
 export default function Home() {
   return (
-    <main className="pl-24">
-      <Suspense>
+    <main className="pt-20 pl-24 flex flex-col min-h-screen">
+      <div className="mb-20">
         <Weather></Weather>
+      </div>
+      <div className="mb-20">
         <News></News>
-        <Photos></Photos>
+      </div>
+
+      <Suspense>
         <OutfitIdeas></OutfitIdeas>
       </Suspense>
-      <div className="flex justify-end pr-24">
+
+      <div className="flex-grow"></div>
+      <div className="flex justify-end pr-24 pb-10">
         <Clock></Clock>
       </div>
       <PageRefreshTimer></PageRefreshTimer>
